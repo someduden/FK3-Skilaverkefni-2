8867de21f907fbab8cbaa485f9cdceff9f565065
someduden <totalynohax@gmail.com>
2026-05-07 18:35:11 +0000

    initial setup and config

---
+++ src/shared/hook/__tests__/useLocalStorage.test.tsx
@@ -0,0 +1,7 @@
+import { describe, it, expect, beforeEach, vi } from 'vitest';
+import { renderHook, act } from '@testing-library/react';
+import { useLocalStorage } from '../useLocalStorage';
+
+describe('useLocalStorage hook', () => {
+  beforeEach(() => {});
+});
